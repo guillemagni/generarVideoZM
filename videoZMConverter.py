81ef39f4efaa3a953171ad006ee48761d6aa1bff
Guillermo Magni <magniguillermo@gmail.com>
2024-12-09 14:32:51 -0300

    Se agrega una barra de carga y algún comentario extra

--- videoZMConverter.py
+++ videoZMConverter.py
@@ -1,6 +1,7 @@
 import cv2
 import os
 import glob
+from tqdm import tqdm
 
 def generarVideo(fotos, video, fps):
     #Revisar el parametro para la terminación del archivo dependiendo que tipo de imagen se quiera utilziar
@@ -13,12 +14,10 @@ def generarVideo(fotos, video, fps):
     frameEjemplo = cv2.imread(originales[0])
     altura, ancho, _ = frameEjemplo.shape
     codecVideo = cv2.VideoWriter_fourcc(*'mp4v')
-
-
-    crearVideo = cv2.VideoWriter(video, codecVideo, fps, (ancho, altura))
+    crearVideo = cv2.VideoWriter(video, codecVideo, fps, (ancho, altura)) #Objeto para crear el video
     print("Creando video..")
 
-    for imagen in originales:
+    for imagen in tqdm(originales, desc="Procesando", unit="frame"):
         frame = cv2.imread(imagen)
         if frame is None:
             print(f"ERROR! -> Error al leer la imagen {imagen}")
@@ -37,7 +36,7 @@ if __name__ == "__main__":
     path_carpeta = "path"
     path_video =  nombre + ".mp4"
     fps = 30
-    
+
     if not os.path.exists(path_carpeta):
         print(f"ERROR! -> La carpeta {path_carpeta} no existe. Modifique el path..")
     else:
